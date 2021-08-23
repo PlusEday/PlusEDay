e1b7b4c5e852cf1cb117d26cf075b5508cc0c7ce
Garit <lleun8956@naver.com>
2021-08-23 22:29:57 +0900

    create: MemberVO.java

---
+++ src/main/java/org/eday/domain/MemberVO.java
@@ -0,0 +1,15 @@
+package org.eday.domain;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	
+	private int member_id;
+	private String member_name;
+	private String email;
+	private String pw;
+	private String nickname;
+	private int deleted_flag;
+
+}
