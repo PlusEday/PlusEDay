74767d33b891bca00f33519c5d49636acb38caec
beom1843 <beom1843@gmail.com>
2021-09-01 19:22:14 +0900

    fix. fixing 404 error on swagger UI(but /v2/api-docs is working)

--- src/main/java/org/eday/config/ServletConfig.java
+++ src/main/java/org/eday/config/ServletConfig.java
@@ -50,8 +50,10 @@ public class ServletConfig implements WebMvcConfigurer {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("org.eday.controller")) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
-				.paths(PathSelectors.ant("/api/**")) // PathSelectors.any() 를 할경우 모든 경로가 다 사용된다. RestController가 아닌 것까지 사용된다.
+				//.apis(RequestHandlerSelectors.basePackage("org.eday.controller")) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+				.apis(RequestHandlerSelectors.any())
+				//.paths(PathSelectors.ant("/api/**")) // PathSelectors.any() 를 할경우 모든 경로가 다 사용된다. RestController가 아닌 것까지 사용된다.
+				.paths(PathSelectors.any())
 				.build()
 				.useDefaultResponseMessages(false);
 	}
