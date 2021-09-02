1676cd8d703e387ad8935ce296f639c17e6a65a9
con <imyhy01@gmail.com>
2021-09-02 14:53:09 +0900

    feat. post 좋아요 api 완성

--- src/main/java/org/eday/service/PostService.java
+++ src/main/java/org/eday/service/PostService.java
@@ -1,8 +1,11 @@
 package org.eday.service;
 
 import org.eday.domain.PostVO;
+import org.eday.domain.Post_likeVO;
 
 public interface PostService {
 
 	public int insertPost(PostVO post);
+	
+	public int postLike(Post_likeVO likeVO);
 }
--- src/main/java/org/eday/service/PostServiceImpl.java
+++ src/main/java/org/eday/service/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package org.eday.service;
 
 import org.eday.domain.PostVO;
+import org.eday.domain.Post_likeVO;
 import org.eday.mapper.PostMapper;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,32 @@ public class PostServiceImpl implements PostService {
 		log.info("InsertPost.......!!!" + post);
 		
 		return mapper.insertPost(post);
+	}
+
+	@Override
+	public int postLike(Post_likeVO likeVO) {
+		log.info("Like Post Service. . . . . ." + likeVO);
+		
+		// 좋아요 여부 체크
+		String checkYN = mapper.selectCheckLikePost(likeVO);
+		
+		// 좋아요를 눌렀을 때 POSTLIKE 테이블에 값이 있고
+		if(checkYN != null){
+			if(likeVO.getLike_toggle().equals("y")) {
+				log.info("좋아요 취소");
+				// 좋아요가 눌려진 상태라면
+				likeVO.setLike_toggle("n");
+				return mapper.updatePostLike(likeVO);
+			} else if(likeVO.getLike_toggle().equals("n")){
+				log.info("다시 좋아요");
+				// 좋아요가 취소된 상태라면
+				likeVO.setLike_toggle("y");
+				return mapper.updatePostLike(likeVO);
+			}
+		}
+		log.info("좋아요 눌러");
+		// 좋아요를 누른 전적이 없다면 데이터 추가
+		return mapper.insertPostLike(likeVO);
 	}
 
 }
